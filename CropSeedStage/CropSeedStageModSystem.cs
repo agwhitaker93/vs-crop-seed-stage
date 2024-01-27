abe3b7eebb0984a62f2026ff4a4c5c610faa2eca
Andrew Whitaker <agwhitaker93@gmail.com>
2024-01-27 21:40:54 +0000

    comment logger out for release

--- CropSeedStage/CropSeedStageModSystem.cs
+++ CropSeedStage/CropSeedStageModSystem.cs
@@ -16,17 +16,17 @@ public class CropSeedStageModSystem : ModSystem
 {
     public static int MAX_SEED_MULTIPLIER = 4;
     public static string MOD_ID = "KoboldRanger.CropSeedStage";
-    public static ILogger LOGGER;
+    //public static ILogger LOGGER;
     public static Random RANDOM = new Random();
     public Harmony harmony;
 
     public override void StartServerSide(ICoreServerAPI api)
     {
-        LOGGER = api.Logger;
+        //LOGGER = api.Logger;
         harmony = new Harmony(MOD_ID);
-        LOGGER.Notification($"{MOD_ID}: Initializing");
+        //LOGGER.Notification($"{MOD_ID}: Initializing");
         harmony.PatchAll();
-        LOGGER.Notification($"{MOD_ID}: Should be done initializing now");
+        //LOGGER.Notification($"{MOD_ID}: Should be done initializing now");
     }
 
     public override void Dispose()
@@ -40,9 +40,9 @@ public class CropSeedStageModSystem : ModSystem
     {
         int cropStage = __instance.CurrentStage();
         int extraSeedStage = __instance.CropProps.GrowthStages - 1;
-        LOGGER.Notification($"{MOD_ID}: Broken crop of stage {cropStage}. Extra seed stage is {extraSeedStage}");
+        //LOGGER.Notification($"{MOD_ID}: Broken crop of stage {cropStage}. Extra seed stage is {extraSeedStage}");
         if (cropStage != extraSeedStage) {
-            LOGGER.Notification($"{MOD_ID}: Crop is not in extra seed stage, leaving early");
+            //LOGGER.Notification($"{MOD_ID}: Crop is not in extra seed stage, leaving early");
             // exit early if it's not the second to last growth stage
             return;
         }
@@ -50,26 +50,26 @@ public class CropSeedStageModSystem : ModSystem
         ItemStack seedStack = null;
         for (int i = 0; i < __result.Length; i++) {
             ItemStack itemStack = __result[i];
-            LOGGER.Notification($"{MOD_ID}: Post BlockCrop.GetDrops[{i}] item: {itemStack.Item} quantity: {itemStack.StackSize}");
+            //LOGGER.Notification($"{MOD_ID}: Post BlockCrop.GetDrops[{i}] item: {itemStack.Item} quantity: {itemStack.StackSize}");
             if (itemStack.Item != null && itemStack.Item is ItemPlantableSeed) {
                 seedStack = itemStack;
             }
         }
 
         if (seedStack == null) {
-            LOGGER.Notification($"TODO {MOD_ID}: BlockCrop did not have an existing seed drop, adding one anyway");
+            //LOGGER.Notification($"TODO {MOD_ID}: BlockCrop did not have an existing seed drop, adding one anyway");
             foreach (BlockDropItemStack stack in __instance.Drops)
             {
                 ItemStack resolvedItemStack = stack.ResolvedItemstack;
-                LOGGER.Notification($"{MOD_ID}: Droppable stack: {resolvedItemStack.Item}");
+                //LOGGER.Notification($"{MOD_ID}: Droppable stack: {resolvedItemStack.Item}");
                 if (resolvedItemStack.Item != null && resolvedItemStack.Item is ItemPlantableSeed) {
-                    LOGGER.Notification($"{MOD_ID}: Crop could have dropped a seed, attempting to add one manually");
+                    //LOGGER.Notification($"{MOD_ID}: Crop could have dropped a seed, attempting to add one manually");
                     seedStack = resolvedItemStack.Clone();
                     seedStack.StackSize = 1;
-                    LOGGER.Notification($"{MOD_ID}: Attempting to create new array");
+                    //LOGGER.Notification($"{MOD_ID}: Attempting to create new array");
                     List<ItemStack> newList = new List<ItemStack>(__result);
                     newList.Add(seedStack);
-                    LOGGER.Notification($"{MOD_ID}: Hoping reassigning __result doesn't just lose our reference and actually changes it");
+                    //LOGGER.Notification($"{MOD_ID}: Hoping reassigning __result doesn't just lose our reference and actually changes it");
                     __result = newList.ToArray();
                 }
             }
@@ -80,6 +80,6 @@ public class CropSeedStageModSystem : ModSystem
         // random.Next lower bound is inclusive, upper down is exclusive
         int newSeedDrop = RANDOM.Next(currentSeedDrop, maxSeedDrop + 1);
         seedStack.StackSize = newSeedDrop;
-        LOGGER.Notification($"{MOD_ID}: Increasing stack size from {currentSeedDrop} to {newSeedDrop}");
+        //LOGGER.Notification($"{MOD_ID}: Increasing stack size from {currentSeedDrop} to {newSeedDrop}");
     }
 }
